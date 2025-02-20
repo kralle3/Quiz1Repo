a9939d2b6461a1555575a75efa36b87592b21b67
kralle3 <kamarirallen@gmail.com>
2025-02-20 15:06:45 -0600

    Green Button WOrking
    Green button now works and turns led green

--- Quiz1/Quiz1.ino
+++ Quiz1/Quiz1.ino
@@ -3,21 +3,26 @@ const int greenPin = 8;
 const int bluePin = 7;
 
 const int buttonRed = 5;
-const int buttonGreen = 4;
-const int buttonBlue = 3;
+const int buttonGreen = 3;
+const int buttonBlue = 4;
 
 void setup() {
     pinMode(redPin, OUTPUT);
-
+    pinMode(greenPin, OUTPUT);
+    
+    
     pinMode(buttonRed, INPUT_PULLUP);
- 
+    pinMode(buttonGreen, INPUT_PULLUP);
+   
 }
 
 void loop() {
     int redState = digitalRead(buttonRed);
-   
+    int greenState = digitalRead(buttonGreen);
+ 
 
     digitalWrite(redPin, !redState);  // Invert logic (LOW means pressed)
+    digitalWrite(greenPin, !greenState);
   
 }
 
