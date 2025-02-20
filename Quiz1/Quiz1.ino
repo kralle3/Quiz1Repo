e2696e9b8665fb8614e98c110ea9aae7dcdde929
kralle3 <kamarirallen@gmail.com>
2025-02-20 15:43:39 -0600

    Finished code
    Finished with working buttons

--- Quiz1/Quiz1.ino
+++ Quiz1/Quiz1.ino
@@ -1,22 +1,23 @@
-const int redPin = 9;
+const int redPin = 9; //sets pins to digital outputs
 const int greenPin = 8;
 const int bluePin = 7;
 
-const int buttonRed = 5;
+const int buttonRed = 5; //set buttons for digital output
 const int buttonGreen = 3;
 const int buttonBlue = 4;
 
 void setup() {
-    pinMode(redPin, OUTPUT);
+    pinMode(redPin, OUTPUT); //set outputs
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
     
     pinMode(buttonRed, INPUT_PULLUP);
-    pinMode(buttonGreen, INPUT_PULLUP);
+    pinMode(buttonGreen, INPUT_PULLUP); //set inputs
     pinMode(buttonBlue, INPUT_PULLUP);
 }
 
 void loop() {
+  
     int redState = digitalRead(buttonRed);
     int greenState = digitalRead(buttonGreen);
     int blueState = digitalRead(buttonBlue);
@@ -24,6 +25,9 @@ void loop() {
     digitalWrite(redPin, !redState); // Invert logic (LOW means pressed)
     digitalWrite(greenPin, !greenState);
     digitalWrite(bluePin, !blueState);
+
+
+    
 }
 
 
